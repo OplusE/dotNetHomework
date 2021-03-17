a707c9a9658297bcfd8bd2dd325737ff4249f520
OplusE <80024939+OplusE@users.noreply.github.com>
2021-03-17 12:03:16 +0800

    Add files via upload

---
+++ ConsoleHomework1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleHomework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int d;
+            char c;
+            a=Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToChar(Console.ReadLine());
+            switch (c) {
+                case '+':
+                    d= a + b;
+                    break;
+                case '-':
+                    d = a - b;
+                    break;
+                case '*':
+                    d = a * b;
+                    break;
+                case '/':
+                    d = a / b;
+                    break;
+                default:
+                    d = 1999;
+                    break;
+
+            
+      
+            }
+
+
+            Console.WriteLine(d);
+        }
+    }
+}
